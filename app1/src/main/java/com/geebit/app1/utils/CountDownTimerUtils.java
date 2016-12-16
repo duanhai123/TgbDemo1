e89b5b3f142eae5092e43797489f371c47aae0bd
duanhai123 <303971866@qq.com>
2016-12-16 09:09:05 +0800

    写了倒计时的工具类

--- app1/src/main/java/com/geebit/app1/utils/CountDownTimerUtils.java
+++ app1/src/main/java/com/geebit/app1/utils/CountDownTimerUtils.java
@@ -33,7 +33,7 @@ public class CountDownTimerUtils extends CountDownTimer {
                 mTextView.getText().toString());
         ForegroundColorSpan span = new ForegroundColorSpan(Color.RED);
         /**
-         * public void setSpan(Object what, int start, int end, int flags) {
+         *
          * 主要是start跟end，start是起始位置,无论中英文，都算一个。
          * 从0开始计算起。end是结束位置，所以处理的文字，包含开始位置，但不包含结束位置。
          */
