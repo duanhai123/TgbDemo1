10c402c9e48aefa7fb611d8cd8b6ec1e97a446b1
duanhai123 <303971866@qq.com>
2016-12-21 23:39:01 +0800

    修复筛选弹出菜单的bug

--- app1/src/main/java/com/geebit/app1/activity/HoldingDetailActivity.java
+++ app1/src/main/java/com/geebit/app1/activity/HoldingDetailActivity.java
@@ -1,9 +1,12 @@
 package com.geebit.app1.activity;
 
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Gravity;
-import android.view.MotionEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -14,11 +17,13 @@ import com.geebit.app1.R;
  * Created by DEll on 2016-12-21
  */
 public class HoldingDetailActivity extends BaseActivity implements View.OnClickListener {
-    private PopupWindow popupWindow;
+
     private View view;
 
     private ImageView mBack;
     private TextView mSort;
+    private PopupWindow window;
+
 
     @Override
     protected void initoView() {
@@ -46,48 +51,45 @@ public class HoldingDetailActivity extends BaseActivity implements View.OnClickL
                 finish();
                 break;
             case R.id.tv_sort:
-                getPopupWindow();
-                popupWindow.showAtLocation(view, Gravity.LEFT, 0, 0);
-                break;
+init();
+            break;
         }
     }
 
-    /**
-     * 创建PopupWindow
-     */
-    protected void initPopuptWindow() {
-        // TODO Auto-generated method stub
-        // 获取自定义布局文件activity_popupwindow_left.xml的视图
-        View popupWindow_view = getLayoutInflater().inflate(R.layout.holding_sort, null,
-                false);
-        // 创建PopupWindow实例,200,LayoutParams.MATCH_PARENT分别是宽度和高度
-        popupWindow = new PopupWindow(popupWindow_view, 200, WindowManager.LayoutParams.MATCH_PARENT, true);
-        // 设置动画效果
-        popupWindow.setAnimationStyle(R.style.Animation);
-        // 点击其他地方消失
-        popupWindow_view.setOnTouchListener(new View.OnTouchListener() {
+    private void init(){
+        // 利用layoutInflater获得View
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.holding_sort, null);
+        window = new PopupWindow(view,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setFocusable(true);
+        // 实例化一个ColorDrawable颜色为半透明
+        ColorDrawable dw = new ColorDrawable(0xb0000000);
+        window.setBackgroundDrawable(dw);
+        // 设置popWindow的显示和消失动画
+        window.setAnimationStyle(R.style.Animation);
+        // 在底部显示
+        window.showAtLocation(HoldingDetailActivity.this.findViewById(R.id.tv_sort),
+                Gravity.TOP, 120, 120);
+        Button first = (Button) view.findViewById(R.id.btn_proceed);
+        first.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // TODO Auto-generated method stub
-                if (popupWindow != null && popupWindow.isShowing()) {
-                    popupWindow.dismiss();
-                    popupWindow = null;
-                }
-                return false;
+            public void onClick(View v) {
+                    window.dismiss();
+                System.out.println("第一个按钮被点击了");
             }
         });
-    }
 
-    /***
-     * 获取PopupWindow实例
-     */
-    private void getPopupWindow() {
-        if (null != popupWindow) {
-            popupWindow.dismiss();
-            return;
-        } else {
-            initPopuptWindow();
-        }
-    }
+        //popWindow消失监听方法
+        window.setOnDismissListener(new PopupWindow.OnDismissListener() {
 
+            @Override
+            public void onDismiss() {
+                System.out.println("popWindow消失");
+            }
+        });
+
+    }
 }
